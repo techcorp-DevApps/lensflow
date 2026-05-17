38a57ef08d01eed2e7ca860e453a76a7dafe8ab1
TechCorp <techcorp2024@gmail.com>
2026-05-17 14:29:18 +1000

    Update collapsible.jsx

--- src/components/ui/collapsible.jsx
+++ src/components/ui/collapsible.jsx
@@ -1,4 +1,11 @@
-// Stub component
-export default function Stub() {
-  return null;
-}
+"use client"
+
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+
+const Collapsible = CollapsiblePrimitive.Root
+
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
+
+const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent }
